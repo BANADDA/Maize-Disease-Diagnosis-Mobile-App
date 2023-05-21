a23f5a9ee9248533ee98f2b86eae0b8694d34643
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2023-05-21 10:42:07 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+import { Button, ThemeProvider } from 'react-native-elements';
+
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+            <ThemeProvider>
+
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+          </ThemeProvider>
+
+      </SafeAreaProvider>
+      
+    );
+  }
+}
